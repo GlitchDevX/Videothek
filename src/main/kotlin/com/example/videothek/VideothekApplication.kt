98c6e0fbec4c284798e4f93c524dc8f5b4a61595
JonasRingeis <jonas.ringeis@otto.de>
2024-12-03 13:25:38 +0100

    Re-Enabled caching

--- src/main/kotlin/com/example/videothek/VideothekApplication.kt
+++ src/main/kotlin/com/example/videothek/VideothekApplication.kt
@@ -4,12 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 
-//@EnableCaching
+@EnableCaching
 @SpringBootApplication
 class VideothekApplication
-
-// start db with:
-// docker run -d --name mongodb -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=Videothek -e MONGO_INITDB_ROOT_PASSWORD=ZpaEVfrd4QBTRgI0bRxbLgjDP7K1w4 mongo
 
 fun main(args: Array<String>) {
 	runApplication<VideothekApplication>(*args)
